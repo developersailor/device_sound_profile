9a2784f290149c6471aac1beaaff7c5cd1deec9d
mehmet fiskindal <mehmetfiskindal@gmail.com>
2024-06-23 21:37:57 +0300

    change/nativecode
    permission_handler add

--- device_sound_profile_ios/ios/Classes/DeviceSoundProfilePlugin.swift
+++ device_sound_profile_ios/ios/Classes/DeviceSoundProfilePlugin.swift
@@ -1,7 +1,9 @@
 import Flutter
 import UIKit
+import Mute
 
 public class DeviceSoundProfilePlugin: NSObject, FlutterPlugin {
+  var str:String = "normal"
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "device_sound_profile_ios", binaryMessenger: registrar.messenger())
     let instance = DeviceSoundProfilePlugin()
@@ -9,6 +11,16 @@ public class DeviceSoundProfilePlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS")
+   switch call.method {
+           case "getRingerMode":
+                Mute.shared.notify = {
+                  [weak self] m in
+                  self?.str = m ? "vibrate" : "normal"
+                 }
+             result(self.str);
+           default:
+             break;
+         }
+     }
   }
-}
+
--- device_sound_profile_ios/ios/device_sound_profile_ios.podspec
+++ device_sound_profile_ios/ios/device_sound_profile_ios.podspec
@@ -14,8 +14,10 @@ Pod::Spec.new do |s|
   s.source           = { :path => '.' }  
   s.source_files = 'Classes/**/*'
   s.dependency 'Flutter'
+  s.dependency 'Mute', '~> 0.6.0'
   s.platform = :ios, '9.0'
-
+  s.vendored_frameworks = 'DeviceSoundProfile.framework'
+  s.ios.preserve_paths = 'DeviceSoundProfile.framework'
   # Flutter.framework does not contain a i386 slice.
   s.pod_target_xcconfig = { 'DEFINES_MODULE' => 'YES', 'EXCLUDED_ARCHS[sdk=iphonesimulator*]' => 'i386' }
   s.swift_version = '5.0'
